a749f71b1935fd92eaacf307d2262e523e975cbe
Janhavi003 <janhavi.hivarekar@kalvium.community>
2024-01-24 17:29:25 +0530

    added successfully

---
+++ react-router/src/pages/About.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+function About() {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <Navbar /> About
+    </div>
+  );
+}
+
+export default About;
